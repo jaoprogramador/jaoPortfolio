789b9da544af52b69ad02f4d797e380db6ef177b
jao <jaoprogramador@gmail.com>
2024-11-24 12:04:06 +0100

    cambios con nuevos proyectos de angular e iconos

--- portfolio-apps-jao/src/components/AppTable.js
+++ portfolio-apps-jao/src/components/AppTable.js
@@ -9,7 +9,9 @@ const apps = [
       description: "Aplicación para emular un rarrito de la compra",
       technology: "React",
       repoUrl: "https://github.com/jaoprogramador/jaoPortfolio",
-      appUrl: "https://shoppingcard-1ry4.onrender.com/"
+      appUrl: "https://shoppingcard-1ry4.onrender.com/",
+      icon: "/img/react-icon.png" 
+
     },
     {
 
@@ -18,7 +20,9 @@ const apps = [
       description: "Aplicación que contine un componente Follow como el de twitter",
       technology: "React",
       repoUrl: "https://github.com/jaoprogramador/jaoPortfolio/tree/master/00-hola-mundo",
-      appUrl: "https://twitterfollow.onrender.com/"
+      appUrl: "https://twitterfollow.onrender.com/",
+      icon: "/img/react-icon.png" 
+
     },
     {
 
@@ -27,7 +31,9 @@ const apps = [
       description: "Aplicación para tomar notas con React",
       technology: "React, Express",
       repoUrl: "https://github.com/tu-usuario/react-notes",
-      appUrl: "https://react-xhzi.onrender.com/"
+      appUrl: "https://react-xhzi.onrender.com/",
+      icon: "/img/react-icon.png" 
+
     },
     {
       name: "Agenda de Personas",
@@ -35,7 +41,9 @@ const apps = [
       description: "Aplicación para gestionar contactos",
       technology: "React, Express",
       repoUrl: "https://github.com/tu-usuario/react-persons",
-      appUrl: "https://reactpersons.onrender.com"
+      appUrl: "https://reactpersons.onrender.com",
+      icon: "/img/react-icon.png" 
+
     },
     {
       name: "Agenda MongoDB",
@@ -43,7 +51,9 @@ const apps = [
       description: "Aplicación para gestionar contactos con front de react y back en Node-Express y MongoDB",
       technology: "React, MongoDB",
       repoUrl: "https://github.com/tu-usuario/react-persons-mongodb",
-      appUrl: "https://persosmongodb.onrender.com/"
+      appUrl: "https://persosmongodb.onrender.com/",
+      icon: "/img/react-icon.png" 
+
     },
     {
       name: "Citas Hospital",
@@ -51,7 +61,9 @@ const apps = [
       description: "Aplicación para gestionar las entradas a un hospital con pacientes con front de react y back en Node-Express y typescript",
       technology: "React, typescript",
       repoUrl: "https://github.com/jaoprogramador/TipescriptPacientesHospital",
-      appUrl: "https://tipescriptpacienteshospital.onrender.com/"
+      appUrl: "https://tipescriptpacienteshospital.onrender.com/",
+      icon: "/img/react-icon.png" 
+
     },
     {
       name: "Usuarios Blogs",
@@ -59,23 +71,60 @@ const apps = [
       description: "Aplicación para gestionar usuarios y sus blogs con react y back en Node-Express, Bootstrap, Redux y MongoDB",
       technology: "React, MongoDB, redux, bootstrap",
       repoUrl: "https://github.com/tu-usuario/react-persons-mongodb",
-      appUrl: "https://mongouserblogsstilosmenuvar.onrender.com/"
+      appUrl: "https://mongouserblogsstilosmenuvar.onrender.com/",
+      icon: "/img/react-icon.png" 
+
     },
+    {
+      name: "Angular Material",
+      imageUrl: "/img/AngularMaterialDrag.jpg",  
+      description: "Aplicación con angular para usar angular material y el componente drag-drop",
+      technology: "Angular, angular material",
+      repoUrl: "https://github.com/jaoprogramador/angular-apps/tree/master/04EJANGULARMATERIAL",
+      appUrl: "https://angular-material-ut7i.onrender.com/",
+      icon: "/img/angular-icon.png" 
+    },
+    {
+      name: "Angular carga perezosa",
+      imageUrl: "/img/AngularCargaPerezosa.jpg",  
+      description: "Aplicación con angular",
+      technology: "Angular carga perezosa para optimizar el rendimiento de carga",
+      repoUrl: "https://github.com/jaoprogramador/angular-apps/tree/master/04ngModules",
+      appUrl: "https://angular-cargaperezosamodulos.onrender.com/",
+      icon: "/img/angular-icon.png" 
+      
+    },
+    {
+      name: "Workspaces",
+      imageUrl: "/img/angularWorkspace.jpg",  
+      description: "Aplicación con angular que está compuesta cpor un workspace con distintos proyectos",
+      technology: "Angular",
+      repoUrl: "https://github.com/jaoprogramador/angular-apps/tree/master/05ngWorkpsace",
+      appUrl: "https://angular-workspaceproyectos.onrender.com",
+      icon: "/img/angular-icon.png" 
+
+    },
+    
     {
       name: "Login Angular",
       imageUrl: "/img/angular-login.jpg",  
       description: "Aplicación con angular v17 logueo que se autentica contra un API Rest que nos proporciona un token de acceso. Hacemos un routing securizado de distintas páginas en las que podemos ver como pasar información entre componentes. La aplicación hace uso de servicios y peticiones HTTP para el login o para obtener respuestas JSON y tratar la info por pantalla.Se hace uso de PIPE, para definir componentes genéricos control de errores, validacion de formularios, componentes de Angular material...",
       technology: "Angular, Firebase",
       repoUrl: "https://github.com/jaoprogramador/MongoUserBlogsStilosMenuVar",
-      appUrl: "https://proyecto-final-angular-42db4.web.app/login"
+      appUrl: "https://proyecto-final-angular-42db4.web.app/login",
+      icon: "/img/angular-icon.png" 
+
     },
+
     {
       name: "Spring Framework",
       imageUrl: "/img/spring-framework.jpg",  
       description: "Spring Boot, Data, Beans, MVC, Security, JWT, OAuth, Swagger, Maven, REST, Postman, Lambda, JUnit, Postgresql, MongoDB",
       technology: "Spring Framework ",
       repoUrl: "https://github.com/jaoprogramador/CursoSpringOpenBootCamp",
-      appUrl: "https://github.com/jaoprogramador/CursoSpringOpenBootCamp"
+      appUrl: "https://github.com/jaoprogramador/CursoSpringOpenBootCamp",
+      icon: "/img/springBoot-icon.png" 
+
     },
     {
       name: "Spring Cloud Microservicios",
@@ -83,7 +132,8 @@ const apps = [
       description: "Servicios Rest, JPA, Api Rest, Config, Eureka Server, Feighn, Hystry, Gateway, Actuator, Loombok, Gradle",
       technology: "Spring Cloud",
       repoUrl: "https://github.com/jaoprogramador/SpringCloudMicroservicios",
-      appUrl: "https://github.com/jaoprogramador/SpringCloudMicroservicios"
+      appUrl: "https://github.com/jaoprogramador/SpringCloudMicroservicios",
+      icon: "/img/springCloud-icon.png" 
     },
     {
       name: "Ejercicios de programación",
@@ -91,7 +141,8 @@ const apps = [
       description: "Ejercicios de programación, BD, web...",
       technology: "HTML ",
       repoUrl: "https://github.com/jaoprogramador/jaoPortfolio/tree/master/jaoprogramadorweb/public",
-      appUrl: "https://jaoprogramadorweb.onrender.com/j2ee/index.htm"
+      appUrl: "https://jaoprogramadorweb.onrender.com/j2ee/index.htm",
+      icon: "/img/jaoprogramador-icon.png" 
     }
   ];
   
@@ -104,23 +155,32 @@ const apps = [
 
   // Mapeo de categorías con sus respectivas tecnologías y títulos
 const categories = [
-  { title: "Aplicaciones en React", filter: "React" },
-  { title: "Aplicaciones en Angular", filter: "Angular" },
-  { title: "Aplicaciones en Spring Boot", filter: "Spring Framework" },
-  { title: "Aplicaciones en Spring Cloud", filter: "Spring Cloud" },
-  { title: "Web jaoprogramador", filter: "HTML" }
+  { title: "Aplicaciones en React", filter: "React", icon: "/img/icons/react-icon.png"  },
+  { title: "Aplicaciones en Angular", filter: "Angular", icon: "/img/icons/angular-icon.png"  },
+  { title: "Aplicaciones en Spring Boot", filter: "Spring Framework", icon:"/img/icons/springboot-icon.png" },
+  { title: "Aplicaciones en Spring Cloud", filter: "Spring Cloud", icon:"/img/icons/springcloud-icon.png" },
+  { title: "Web jaoprogramador", filter: "HTML", icon:"/img/icons/html-icon.png"  }
 ];
   
 const AppTable = () => {
   return (
     <table className="app-table">
-      {categories.map(({ title, filter }) => {
+      {categories.map(({ title, filter, icon }) => {
         const filteredApps = apps.filter(app => app.technology.includes(filter));
         return (
           <React.Fragment key={title}>
             <thead>
               <tr>
-                <th>{title}</th>
+                <th>
+                  <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                    {title}
+                    <img 
+                      src={icon} 
+                      alt={`${title} icon`} 
+                      style={{ width: '24px', height: '24px' }} 
+                    />
+                  </div>
+                </th>
               </tr>
             </thead>
             <tbody>
