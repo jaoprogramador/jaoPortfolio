047645ca6df9bf183962b12342d85bb21c01c9ee
jao <jaoprogramador@gmail.com>
2025-01-10 15:06:59 +0100

    nueva version de portfolio JAO v1

--- portfolio-apps-jao/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: 'dist',  // Asegúrate de que esta línea está presente
-    }
-});
