7b55b8e4c65190bb627675fa8e06511a4bff46b8
jao <jaoprogramador@gmail.com>
2025-01-10 18:48:07 +0100

    Redeploy 2

--- portfolio-apps-jao/next.config.js
+++ portfolio-apps-jao/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  reactStrictMode: true,  // Mantén el modo estricto de React
+  output: 'standalone',   // Configura el sitio para ser exportado de forma estática
+  trailingSlash: true,     // Agrega una barra diagonal a las URLs
+  images: {
+    unoptimized: true,     // Desactiva la optimización de imágenes si no se soporta en Render
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
